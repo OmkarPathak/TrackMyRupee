6ff8a87238ccb9c44cb362d995442f3b0f44d2df
Omkar Pathak <omkarpathak27@gmail.com>
2026-08-06 18:35:37 +0530

    Sort transactions by date and creation time in descending order

--- expenses/views/all_transactions.py
+++ expenses/views/all_transactions.py
@@ -160,7 +160,7 @@ class AllTransactionsListView(HtmxPartialTemplateMixin, LoginRequiredMixin, List
         elif sort_by == 'amount_desc':
             queryset = queryset.order_by('-unified_amount')
         else:
-            queryset = queryset.order_by('-date')
+            queryset = queryset.order_by('-date', '-created_at')
 
         return queryset
 
